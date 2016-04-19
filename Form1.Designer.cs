f33cf2480540ef4a8ec0a14934127d43fc4057e4
JustynaKaw <justyna6321@wp.pl>
2016-04-19 13:31:45 +0200

    projekt

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace warcaby
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ibProcessed = new Emgu.CV.UI.ImageBox();
+            this.ibOriginal = new Emgu.CV.UI.ImageBox();
+            this.btnPausseorResume = new System.Windows.Forms.Button();
+            this.txtXYZPromien = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ibProcessed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ibOriginal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ibProcessed
+            // 
+            this.ibProcessed.Location = new System.Drawing.Point(671, 12);
+            this.ibProcessed.Name = "ibProcessed";
+            this.ibProcessed.Size = new System.Drawing.Size(640, 480);
+            this.ibProcessed.TabIndex = 2;
+            this.ibProcessed.TabStop = false;
+            // 
+            // ibOriginal
+            // 
+            this.ibOriginal.Location = new System.Drawing.Point(12, 22);
+            this.ibOriginal.Name = "ibOriginal";
+            this.ibOriginal.Size = new System.Drawing.Size(640, 480);
+            this.ibOriginal.TabIndex = 2;
+            this.ibOriginal.TabStop = false;
+            // 
+            // btnPausseorResume
+            // 
+            this.btnPausseorResume.Location = new System.Drawing.Point(12, 552);
+            this.btnPausseorResume.Name = "btnPausseorResume";
+            this.btnPausseorResume.Size = new System.Drawing.Size(132, 44);
+            this.btnPausseorResume.TabIndex = 3;
+            this.btnPausseorResume.Text = "PAUZA";
+            this.btnPausseorResume.UseVisualStyleBackColor = true;
+            this.btnPausseorResume.Click += new System.EventHandler(this.btnPausseorResume_Click);
+            // 
+            // txtXYZPromien
+            // 
+            this.txtXYZPromien.ForeColor = System.Drawing.Color.YellowGreen;
+            this.txtXYZPromien.Location = new System.Drawing.Point(184, 509);
+            this.txtXYZPromien.Multiline = true;
+            this.txtXYZPromien.Name = "txtXYZPromien";
+            this.txtXYZPromien.ReadOnly = true;
+            this.txtXYZPromien.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtXYZPromien.Size = new System.Drawing.Size(468, 111);
+            this.txtXYZPromien.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(671, 498);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(209, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(671, 541);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1326, 632);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtXYZPromien);
+            this.Controls.Add(this.btnPausseorResume);
+            this.Controls.Add(this.ibOriginal);
+            this.Controls.Add(this.ibProcessed);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ibProcessed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ibOriginal)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox ibProcessed;
+        private Emgu.CV.UI.ImageBox ibOriginal;
+        private System.Windows.Forms.Button btnPausseorResume;
+        private System.Windows.Forms.TextBox txtXYZPromien;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,170 @@
+﻿// Rozpoznawanie obrazu z gry warcaby oraz wizualizacja gry na komputerze .
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+
+using OpenTK;
+
+
+namespace warcaby
+{
+
+    public partial class Form1 : Form
+    {
+        //zmienne 
+     // public Tess3D window; 
+       // global
+        
+        Capture capWebcam = null; // kamera 
+        bool przechwytywanie = false;
+        Image<Bgr, Byte> zdjOryginalne;
+        Image<Gray, Byte> zdjTworzone;
+        
+        public Form1()
+        {
+            InitializeComponent();
+           // GameWindow window = new GameWindow(640, 480);
+           // window.Run();
+            //this.window
+            // gl 
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try 
+            {
+                capWebcam = new Capture();   // przechwytywanie 
+            }
+            catch (NullReferenceException except)
+            {
+                txtXYZPromien.Text = except.Message; 
+
+                return; 
+            }                                     
+              
+            //gdy mamy wlasciwy obiekt do przechwycenia
+            //dodajemy funkcje obrazu do listy zadań aplikacji
+           
+            Application.Idle += procesRamkaIAktualizacjaGUI;   // wystąienie zdarzenia - pojawienie się przedmiotu przed ramką - wywolanie funkcji  
+            przechwytywanie = true;                           //sczytywanie obrazu przez kamerę i aktualizacja zmiennej flagi - przechwytujemy 
+
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (capWebcam != null)
+            {
+                capWebcam.Dispose(); 
+            }
+        }
+
+        void procesRamkaIAktualizacjaGUI(object sender, EventArgs arg)
+        {
+
+            zdjOryginalne = capWebcam.QueryFrame();  // do zdj wczytywanie obrazu przechwyconego z kamery 
+            if (zdjOryginalne == null)
+                return;
+            zdjTworzone = zdjOryginalne.InRange(new Bgr(0, 0, 175), new Bgr(100, 100, 256)); //wartość minimalna i max filtru (jeśli kolor jest większy niż lub równy tej wartości)
+                                             
+            // InRange sprawdza czy elementy obrazu leżą pomiędza dwoma zmiennymi skalarnymi
+            // TColor <byte> {lower hihger} - TColor byte
+            zdjTworzone = zdjTworzone.SmoothGaussian(9);
+
+            CircleF[] circles = zdjTworzone.HoughCircles(new Gray(100), new Gray(50), 2, zdjTworzone.Height/4, 10, 400) [0];
+            //  100 prog Canny - Canny edge detector - operator wykrywania krawędzi wykorzystujacy alg wielostopniowyw w celu wykrycia szeregu krawędzi 
+            //   zdjTworzone.Height/4 -  min odległość w pikselach między ośrodkami wykrytych kręgach
+            // min i max promień wykrytego okręgu koła się z pierwszego kanału
+            foreach (CircleF circle in circles)
+            {
+                if (txtXYZPromien.Text != "")
+                    txtXYZPromien.AppendText(Environment.NewLine);
+                txtXYZPromien.AppendText("Pozycja pilki : X = " + circle.Center.X.ToString().PadLeft(4) +       // x i y pozycji srodka okregu
+                                        " , Y = " + circle.Center.Y.ToString().PadLeft(4) +
+                                        " , Promien = " + circle.Radius.ToString("###.000").PadLeft(7));
+
+                txtXYZPromien.ScrollToCaret();  // przesunąć pasek przewijania w dół pola tekstowego  
+
+                // rysowanie  małego zielonego kółka w środku wykrytego obiektu 
+                // rysowanie okręgu o promieniu 3, mimo że wielkość wykrytego koła będzie znacznie większa
+                // obiekty CvInvoke mogą zostać wykorzystane do wywołania innych funkcji OpenCV 
+                CvInvoke.cvCircle(zdjOryginalne, new Point ((int)circle.Center.X, (int)circle.Center.Y ),3, new MCvScalar(0, 255, 0), -1, LINE_TYPE.CV_AA,0) ;
+                //Grubość koła w pikselach, -1 wskazuje aby wypełnić koło ; AA -wygładzenie pikseli, 0 - bez przesuniecia 
+                zdjOryginalne.Draw(circle, new Bgr(Color.Red), 3); // narysowac czerwone kolo wokol wykrytego obiektu 
+                
+
+                
+            }
+
+            ibOriginal.Image = zdjOryginalne;
+            ibProcessed.Image = zdjTworzone;
+        }
+
+
+        private void btnPausseorResume_Click(object sender, EventArgs e)
+        {
+            if (przechwytywanie == true)
+            {
+                Application.Idle -= procesRamkaIAktualizacjaGUI; // usun f.obrazu w liscie zadan aplikacji 
+                przechwytywanie = false; // zmien znacznik - flage 
+                btnPausseorResume.Text = "WZNÓW"; 
+
+            }
+            else 
+            {
+                Application.Idle += procesRamkaIAktualizacjaGUI;
+                przechwytywanie = true; 
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GameWindow window = new GameWindow(640, 480);
+            window.Run();
+
+        }
+
+
+        // open gl
+
+
+      /* public Tess3D():base()
+        {
+            LightAmbient = new float[4];
+            LightAmbient[0] = 1.0f;
+            LightAmbient[1] = 1.0f;
+            LightAmbient[2] = 1.0f;
+            LightAmbient[3] = 1.0f;
+            LightDiffuse = new float[4];
+            LightDiffuse[0] = 1.0f;
+            LightDiffuse[1] = 1.0f;
+            LightDiffuse[2] = 1.0f;
+            LightDiffuse[3] = 1.0f;
+            LightPosition = new float[4];
+            LightPosition[0] = 0.0f;
+            LightPosition[1] = 0.0f;
+            LightPosition[2] = -1.0f;
+            LightPosition[3] = 1.0f;
+            this.Textures = new uint[file.Length];
+        }*/
+        //protected override void InitGLContext()
+        //{ }
+    }
+
+      //public Tess3D(): base()
+       // {}
+}
